23c3a2342c6ccd3796bc18b6b4135e15d67ee5c2
Brooks Lybrand <lybrand.brooks@heb.com>
2021-08-30 17:19:25 -0500

    Add example

---
+++ example/README.md
@@ -0,0 +1,1 @@
+This example was taken from a talk I did on [Large State Optimizations in React](https://github.com/brookslybrand/react-large-state-optimization)
---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import Accordions from "./accordions";
+
+import { FormsStateProvider } from "./forms-context";
+
+const OptimizationApp1 = () => (
+  <FormsStateProvider>
+    <div style={{ width: "50%", margin: "2rem auto" }}>
+      <Accordions />
+    </div>
+  </FormsStateProvider>
+);
+
+export default OptimizationApp1;
